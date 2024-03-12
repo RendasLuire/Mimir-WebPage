39cf4494a620dc2c73fb3ba40afc57cd1445bf71
RendasLuire <andres.aguirre_o@hotmail.com>
2024-03-12 16:14:05 -0600

    change modal structure

--- src/Components/AddButton.jsx
+++ src/Components/AddButton.jsx
@@ -1,27 +1,12 @@
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import UserForm from './UsersComponents/UserForm';
 
-const AddButton = ({title, content}) => {
+const AddButton = () => {
     return (
         <div className="card shadow p-3">
             <div className="container">
-                <button className="btn btn-outline-primary" type="button" data-bs-toggle="modal" data-bs-target="#Modal"><AddCircleOutlineOutlinedIcon /> </button>
-                <div className='modal fade' id="Modal" aria-labelledby="ModalLabel" aria-hidden="true">
-                    <div className="modal-dialog">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h1 className="modal-title fs-5" id="exampleModalLabel">Add {title}</h1>
-                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div className="modal-body">
-                                {content}
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
-                                <button type="button" className="btn btn-primary">Save</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <button className="btn btn-outline-primary" type="button" data-bs-toggle="modal" data-bs-target="#UserModal"><AddCircleOutlineOutlinedIcon /> </button>
+                <UserForm />
             </div>
         </div>
         )
--- src/Components/UsersComponents/UserForm.jsx
+++ src/Components/UsersComponents/UserForm.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
-
 const UserForm = () => {
   return (
-    <div>
-      
+    <div className='modal fade' id="UserModal" aria-labelledby="ModalLabel" aria-hidden="true">
+      <div className="modal-dialog">
+        <div className="modal-content">
+            <div className="modal-header">
+                <h1 className="modal-title fs-5" id="exampleModalLabel">Add User</h1>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <form>
+                <div className='mb-3'>
+                  <label className="form-label">Name</label>
+                  <input type="text" className="form-control" placeholder="Name"/>
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">Deparment</label>
+                  <input type="text" className="form-control" placeholder="Deparment"/>
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">Position</label>
+                  <input type="text" className="form-control" placeholder="Position" />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">Manager</label>
+                  <input type="text" className="form-control" placeholder="Manager" />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">ManagerPosition</label>
+                  <input type="text" className="form-control" placeholder="ManagerPosition" />
+                </div>
+                <div className="modal-footer">
+                    <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
+                    <button type="button" className="btn btn-primary">Save</button>
+                </div>
+              </form>
+              </div>
+            </div>
+        </div>
     </div>
   )
 }
