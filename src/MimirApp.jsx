91d4f1fc467f77385125916d3598d1dd92aa054e
RendasLuire <andres.aguirre_o@hotmail.com>
2024-04-01 17:12:02 -0600

    add login

--- src/MimirApp.jsx
+++ src/MimirApp.jsx
@@ -1,31 +1,36 @@
-import { Route, Routes, Navigate } from "react-router-dom"
-import NavBar from "./Components/NavBar"
-import HomePage from "./pages/HomePage"
-import UsersPage from "./pages/UsersPage"
-import ComputersPage from "./pages/ComputersPage"
-import MonitorsPage from "./pages/MonitorsPage"
-import PrintersPage from "./pages/PrintersPage"
-import AnnexedsPage from "./pages/AnnexedsPage"
-import SettingsPage from "./pages/SettingsPage"
+import { Route, Routes, Navigate } from "react-router-dom";
+import NavBar from "./Components/NavBar";
+import HomePage from "./pages/HomePage";
+import UsersPage from "./pages/UsersPage";
+import ComputersPage from "./pages/ComputersPage";
+import MonitorsPage from "./pages/MonitorsPage";
+import PrintersPage from "./pages/PrintersPage";
+import AnnexedsPage from "./pages/AnnexedsPage";
+import SettingsPage from "./pages/SettingsPage";
+import LogInPage from "./pages/LogInPage";
+import AuthProvider from "./context/AuthProvider";
 
 const MimirApp = () => {
   return (
     <>
       <NavBar />
       <div className="container">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/users" element={<UsersPage /> } />
-          <Route path="/computers" element={<ComputersPage />} />
-          <Route path="/monitors" element={<MonitorsPage />} />
-          <Route path="/printers" element={<PrintersPage /> } />
-          <Route path="/annexeds" element={<AnnexedsPage />} />
-          <Route path="/settings" element={<SettingsPage />} />
-          <Route path="/*" element={<Navigate to={"/"}/>} />
-        </Routes>
+        <AuthProvider>
+          <Routes>
+            <Route path="/login" element={<LogInPage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/users" element={<UsersPage />} />
+            <Route path="/computers" element={<ComputersPage />} />
+            <Route path="/monitors" element={<MonitorsPage />} />
+            <Route path="/printers" element={<PrintersPage />} />
+            <Route path="/annexeds" element={<AnnexedsPage />} />
+            <Route path="/settings" element={<SettingsPage />} />
+            <Route path="/*" element={<Navigate to={"/"} />} />
+          </Routes>
+        </AuthProvider>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default MimirApp
+export default MimirApp;
--- src/pages/LogInPage.jsx
+++ src/pages/LogInPage.jsx
@@ -1,9 +1,47 @@
+import useForm from "../Components/useForm"
+import Global from "../helpers/Global"
 
 const LogInPage = () => {
+
+  const {formState, onInputChange} = useForm({})
+
+  const loginUser = async (e) => {
+    e.preventDefault()
+
+    let userToLogin = formState
+
+    console.log(userToLogin)
+
+    const request = await fetch(Global.url+"users/login", {
+      method: "POST",
+      body:JSON.stringify(userToLogin),
+      headers: {
+        "Content-Type":"application/json"
+      }
+    })
+
+    const data = await request.json();
+
+    console.log(data)
+
+    localStorage.setItem("token", data.token)
+    localStorage.setItem("user", JSON.stringify(data.user))
+  }
+
   return (
-    <div>
-      
-    </div>
+    <>
+        <form className='' onSubmit={loginUser}>
+            <div>
+                <label htmlFor="nickname" >Nickname</label>
+                <input type='text' name='nickname' onChange={onInputChange}/>
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input type='password' name='password' onChange={onInputChange}/>
+            </div>
+            <input type='submit' value={"Identificate"} className='btn btn-success' />
+        </form>
+    </>
   )
 }
 
