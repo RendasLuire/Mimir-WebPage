aa4084c9f9aa78c528ba6592c7ad71d183b8f1a4
Andres Uriel Aguirre Ocampo <andres.aguirre_o@hotmail.com>
2025-02-14 17:38:51 -0600

    fix style for devices

--- src/pages/device/InventoryDevices/InventoryDevices.jsx
+++ src/pages/device/InventoryDevices/InventoryDevices.jsx
@@ -107,7 +107,7 @@ const InventoryDevices = () => {
                   {devices.map((device) => (
                     <div
                       key={device._id}
-                      className="col-xs-12 col-sm-6 col-md-4 col-lg-3"
+                      className="col-12 col-sm-6 col-md-4 col-lg-3 d-flex justify-content-center"
                     >
                       <CardDevice device={device} />
                     </div>
